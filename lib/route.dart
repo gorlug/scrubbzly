b43429bc0dae99b96b90e8afa972d6015efb1b2a
Achim Rohn <achim@rohn.eu>
2022-12-30 17:17:20 +0100

    Initial commit

---
+++ lib/route.dart
@@ -0,0 +1,109 @@
+/// Board
+///
+/// X|X|X|X|X
+/// x|O|O|O|X
+/// E|O|O|O|S
+/// X|O|O|O|X
+/// X|X|X|X|X
+///
+/// X|X|X|X|X
+/// X|O|┏|┓|X
+/// E|━|┛|┗|S
+/// X|O|O|O|X
+/// X|X|X|X|X
+///
+/// X|X|X|X|X
+/// X|O|┏|┓|X
+/// E|┓|┃|┗|S
+/// X|┗|┛|O|X
+/// X|X|X|X|X
+///
+///
+
+mixin Block {
+  String toChar();
+}
+
+class RouteBlock with Block {
+  BlockSide start;
+  BlockSide? end;
+
+  RouteBlock(this.start);
+
+  @override
+  String toChar() {
+    if (start == BlockSide.left && end == BlockSide.bottom) {
+      return RouteChar.leftBottom.char;
+    }
+    if (start == BlockSide.bottom && end == BlockSide.left) {
+      return RouteChar.leftBottom.char;
+    }
+    if (start == BlockSide.left && end == BlockSide.top) {
+      return RouteChar.leftTop.char;
+    }
+    if (start == BlockSide.top && end == BlockSide.left) {
+      return RouteChar.leftTop.char;
+    }
+    if (start == BlockSide.right && end == BlockSide.bottom) {
+      return RouteChar.rightBottom.char;
+    }
+    if (start == BlockSide.bottom && end == BlockSide.right) {
+      return RouteChar.rightBottom.char;
+    }
+    if (start == BlockSide.right && end == BlockSide.top) {
+      return RouteChar.rightTop.char;
+    }
+    if (start == BlockSide.top && end == BlockSide.right) {
+      return RouteChar.rightTop.char;
+    }
+    if (start == BlockSide.left && end == BlockSide.right) {
+      return RouteChar.leftRight.char;
+    }
+    if (start == BlockSide.right && end == BlockSide.left) {
+      return RouteChar.leftRight.char;
+    }
+    if (start == BlockSide.top && end == BlockSide.bottom) {
+      return RouteChar.topBottom.char;
+    }
+    return RouteChar.topBottom.char;
+  }
+}
+
+class Route {
+  List<RouteBlock> _blocks = [];
+}
+
+enum BlockSide {
+  left,
+  right,
+  bottom,
+  top,
+}
+
+enum RouteChar {
+  leftBottom,
+  leftTop,
+  rightBottom,
+  rightTop,
+  leftRight,
+  topBottom,
+}
+
+extension RouteCharExtension on RouteChar {
+  String get char {
+    switch (this) {
+      case RouteChar.leftBottom:
+        return '┓';
+      case RouteChar.leftTop:
+        return '┛';
+      case RouteChar.rightBottom:
+        return '┏';
+      case RouteChar.rightTop:
+        return '┗';
+      case RouteChar.leftRight:
+        return '━';
+      case RouteChar.topBottom:
+        return '┃';
+    }
+  }
+}
---
+++ lib/route_test.dart
@@ -0,0 +1,60 @@
+import 'package:jira_game/route.dart';
+import 'package:test/test.dart';
+
+void expectRouteChar(BlockSide start, BlockSide end, String expectedChar) {
+  final block = RouteBlock(start);
+  block.end = end;
+  expect(block.toChar(), expectedChar);
+}
+
+void main() {
+  group('map route block to character', () {
+    test('should map left bottom', () {
+      expectRouteChar(BlockSide.left, BlockSide.bottom, RouteChar.leftBottom.char);
+    });
+
+    test('should map bottom left', () {
+      expectRouteChar(BlockSide.bottom, BlockSide.left, RouteChar.leftBottom.char);
+    });
+
+    test('should map left top', () {
+      expectRouteChar(BlockSide.left, BlockSide.top, RouteChar.leftTop.char);
+    });
+
+    test('should map top left', () {
+      expectRouteChar(BlockSide.top, BlockSide.left, RouteChar.leftTop.char);
+    });
+
+    test('should map right bottom', () {
+      expectRouteChar(BlockSide.right, BlockSide.bottom, RouteChar.rightBottom.char);
+    });
+
+    test('should map bottom right', () {
+      expectRouteChar(BlockSide.bottom, BlockSide.right, RouteChar.rightBottom.char);
+    });
+
+    test('should map right top', () {
+      expectRouteChar(BlockSide.right, BlockSide.top, RouteChar.rightTop.char);
+    });
+
+    test('should map top right', () {
+      expectRouteChar(BlockSide.top, BlockSide.right, RouteChar.rightTop.char);
+    });
+
+    test('should map left right', () {
+      expectRouteChar(BlockSide.left, BlockSide.right, RouteChar.leftRight.char);
+    });
+
+    test('should map right left', () {
+      expectRouteChar(BlockSide.right, BlockSide.left, RouteChar.leftRight.char);
+    });
+
+    test('should map top bottom', () {
+      expectRouteChar(BlockSide.top, BlockSide.bottom, RouteChar.topBottom.char);
+    });
+
+    test('should map bottom top', () {
+      expectRouteChar(BlockSide.bottom, BlockSide.top, RouteChar.topBottom.char);
+    });
+  });
+}
